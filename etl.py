b18b9b04ba4ca744764f7efc547dbe2af08c3a86
Eddie Perez <erp_perez@hotmail.com>
2019-06-23 20:20:25 -0700

    added country_de column to earthquakes table

--- etl.py
+++ etl.py
@@ -97,7 +97,7 @@ def extract_transform_load():
     #################################################
     # CREATE TABLES
     #################################################
-    cursor.execute("CREATE TABLE IF NOT EXISTS earthquakes (id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, magnitude VARCHAR(255), place VARCHAR(255), time VARCHAR(255), timezone VARCHAR(255), url VARCHAR(255), tsunami INT(1), ids VARCHAR(255), specific_type VARCHAR(255), geometry VARCHAR(255), lat DECIMAL(10, 6), lng DECIMAL(10,6), depth DECIMAL(6,2)) ENGINE=InnoDB")
+    cursor.execute("CREATE TABLE IF NOT EXISTS earthquakes (id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, magnitude VARCHAR(255), place VARCHAR(255), time VARCHAR(255), timezone VARCHAR(255), url VARCHAR(255), tsunami INT(1), ids VARCHAR(255), specific_type VARCHAR(255), geometry VARCHAR(255), country_de varchar(80), lat DECIMAL(10, 6), lng DECIMAL(10,6), depth DECIMAL(6,2)) ENGINE=InnoDB")
     
     # Create table significant earthquake table
     cursor.execute("CREATE TABLE IF NOT EXISTS significant_earthquakes (db_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, id VARCHAR(255), yr INT(5), month INT(3), day INT(3), hr INT(3), minute INT(2), eq_mag_primary DECIMAL(4,2), intensity VARCHAR(255), country VARCHAR(255), location_name VARCHAR(255), lat DECIMAL(10, 6), lng DECIMAL(10,6), deaths INT(10), damage_millions VARCHAR(255), total_deaths INT(10), total_injuries VARCHAR(255), total_damage_millions VARCHAR(255)) ENGINE=InnoDB")
@@ -136,6 +136,146 @@ def extract_transform_load():
 
     ## executing the query with values
     cursor.executemany(query, values)
+    
+    #country_de column: use case statement to decode geometry column
+    cursor.execute("update earthquakes set country_de =\
+      case when trim(upper(substring_index(geometry, ',', -1))) = 'AL' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'AK' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'AZ' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'AR' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'CA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'CO' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'CT' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'DE' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'DC' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'FL' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'GA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'HI' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'ID' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'IL' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'IN' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'IA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'KS' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'KY' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'LA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'ME' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MD' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MI' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MN' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MS' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MO' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MT' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'NE' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'NV' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'NH' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'NJ' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'NM' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'NY' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'NC' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'ND' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'OH' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'OK' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'OR' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'PA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'RI' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'SC' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'SD' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'TN' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'TX' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'UT' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'VT' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'VA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'WA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'WV' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'WI' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'WY' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'PR' then 'United States'     \
+           when trim(upper(substring_index(geometry, ',', -1))) = 'ALABAMA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'ALASKA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'ARIZONA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'ARKANSAS' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'CALIFORNIA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'COLORADO' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'CONNECTICUT' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'DELAWARE' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'DISTRICT OF COLUMBIA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'FLORIDA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'GEORGIA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'HAWAII' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'IDAHO' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'ILLINOIS' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'INDIANA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'IOWA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'KANSAS' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'KENTUCKY' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'LOUISIANA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MAINE' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MARYLAND' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MASSACHUSETTS' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MICHIGAN' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MINNESOTA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MISSISSIPPI' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MISSOURI' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MONTANA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'NEBRASKA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'NEVADA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'NEW HAMPSHIRE' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'NEW JERSEY' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'NEW MEXICO' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'NEW YORK' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'NORTH CAROLINA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'NORTH DAKOTA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'OHIO' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'OKLAHOMA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'OREGON' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'PENNSYLVANIA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'RHODE ISLAND' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'SOUTH CAROLINA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'SOUTH DAKOTA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'TENNESSEE' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'TEXAS' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'UTAH' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'VERMONT' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'VIRGINIA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'WASHINGTON' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'WEST VIRGINIA' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'WISCONSIN' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'WYOMING' then 'United States'\
+           when trim(upper(substring_index(geometry, ',', -1))) = 'PUERTO RICO' then 'United States' \
+           when trim(upper(substring_index(geometry, ',', -1))) = 'MX' then 'Mexico'\
+           when geometry like '%Off the coast of Oregon%' then 'Off the coast of Oregon'\
+           when geometry like '%Carlsberg Ridge%' then 'Carlsberg Ridge'\
+           when geometry like '%South of the Fiji Islands%' then 'South of the Fiji Islands'\
+           when geometry like '%Kuril Islands%' then 'Kuril Islands'\
+           when geometry like '%Fiji region%' then 'Fiji region'  \
+           when geometry like '%Northern Mid-Atlantic Ridge%' then 'Northern Mid-Atlantic Ridge'  \
+           when geometry like '%Southern Mid-Atlantic Ridge%' then 'Southern Mid-Atlantic Ridge'\
+           when geometry like '%West Chile Rise%' then 'West Chile Rise'\
+           when geometry like '%Central Mid-Atlantic Ridge%' then 'Central Mid-Atlantic Ridge'\
+           when geometry = 'M 3.7 - Gulf of Alaska' then 'Gulf of Alaska'\
+           when geometry = 'M 4.0 - Banda Sea' then 'Banda Sea'\
+           when geometry = 'M 4.2 - North Atlantic Ocean' then 'North Atlantic Ocean'\
+           when geometry = 'M 4.3 - Western Indian-Antarctic Ridge' then 'Western Indian-Antarctic Ridge'\
+           when geometry = 'M 4.4 - Central East Pacific Rise' then 'Central East Pacific Rise'\
+           when geometry = 'M 4.4 - North of Svalbard' then 'North of Svalbard'\
+           when geometry = 'M 4.4 - Off the coast of Central America' then 'Off the coast of Central America'\
+           when geometry = 'M 4.4 - Reykjanes Ridge' then 'Reykjanes Ridge'\
+           when geometry = 'M 4.5 - Mid-Indian Ridge' then 'Mid-Indian Ridge'\
+           when geometry = 'M 4.5 - Southern East Pacific Rise' then 'Southern East Pacific Rise'\
+           when geometry = 'M 4.6 - Greenland Sea' then 'Greenland Sea'\
+           when geometry = 'M 4.6 - North of Ascension Island' then 'North of Ascension Island'\
+           when geometry = 'M 4.6 - Northern East Pacific Rise' then 'Northern East Pacific Rise'\
+           when geometry = 'M 4.7 - South Shetland Islands' then 'South Shetland Islands'\
+           when geometry = 'M 4.7 - Southeast of Easter Island' then 'Southeast of Easter Island'\
+           when geometry = 'M 4.9 - Bouvet Island region' then 'Bouvet Island region'\
+           when geometry = 'M 5.0 - Prince Edward Islands region' then 'Prince Edward Islands region'\
+           when geometry = 'M 5.0 - Southeast Indian Ridge' then 'Southeast Indian Ridge'\
+           when geometry = 'M 5.0 - Vanuatu region' then 'Vanuatu region'\
+           when geometry = 'M 5.1 - South of the Kermadec Islands' then 'South of the Kermadec Islands'\
+           when geometry = 'M 5.2 - Pacific-Antarctic Ridge' then 'Pacific-Antarctic Ridge'\
+    else trim(substring_index(geometry, ',', -1))\
+    end")  
 
     ## to make final output we have to run the 'commit()' method of the database object
     db.commit()
