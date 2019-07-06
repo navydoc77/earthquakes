6b0f6965d66b1d56f234a01c98a22727aa76daea
jesusgom28 <jesusgomezm9728@gmail.com>
2019-07-06 00:21:53 -0700

    working on map and php

--- static/js/warnings_app.js
+++ static/js/warnings_app.js
@@ -1,4 +1,4 @@
-function createMap(eqLayers, timelineLayer, legend) {
+function createMap(eqLayers) {
 
     var eqMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
@@ -17,6 +17,10 @@ function createMap(eqLayers, timelineLayer, legend) {
         "World Map": eqMap,
     };
 
+    var layersControl = L.control.layers(baseMaps, eqLayers, {
+        collapsed: false
+    }).addTo(map);
+    
     // Add tool tip and pop up information to each earthquake marker.
     function addPopupInfo(feature, layer) {
 
@@ -26,25 +30,78 @@ function createMap(eqLayers, timelineLayer, legend) {
         }
     }
 
-    function mapWarnings() {
 
-        d3.json('/api/warnings').then((warningsData) => {
-        
-            // Create a legend.
-            var legend  = L.control({position: 'topleft'}),
-                magBins = d3.ticks(Math.floor(minMaxEQ[0]), minMaxEQ[1], Math.ceil(minMaxEQ[1] - minMaxEQ[0]));
-        
-            // Implement the 'onAdd()' function.
-            legend.onAdd = function (map) {
-        
-                var div = L.DomUtil.create('div', 'info legend');
-        
-                for (var i = 0; i < magBins.length; i++) {
-                    div.innerHTML +=
-                        '<i style="background: ' + colorScale(magBins[i]) + '"></i> ' +
-                        magBins[i] + (magBins[i + 1] ? '&ndash;' + magBins[i + 1] + '<br>' : '+');
-                }
-        
-                return div;
+function mapWarnings() {
+
+    // Query the DB for the geojson earthquake data.
+    d3.json('/api/warnings').then((warningsData) => {
+        // Implement the 'onAdd()' function.
+        legend.onAdd = function (map) {
+    
+            var div = L.DomUtil.create('div', 'info legend');
+    
+            for (var i = 0; i < magBins.length; i++) {
+                div.innerHTML +=
+                    '<i style="background: ' + colorScale(magBins[i]) + '"></i> ' +
+                    magBins[i] + (magBins[i + 1] ? '&ndash;' + magBins[i + 1] + '<br>' : '+');
             }
+    
+            return div;
+        }
+    
+    
+        // Initialize an object used to hold the earthquake layers.
+        var eqLayers = {};
+    
+        for (var i = 0; i < magBins.length; i++) {
+    
+            // Create an overlay layer of earthquake markers for quakes within a magnitude range.
+            var eqLayer = L.geoJSON(warningsData.features, {
+                        filter: function (feature) {
+                            return (i == magBins.length - 1 ?
+                                    (+feature.properties.event >= magBins[i]) :
+                                    (+feature.properties.senderName >= magBins[i])  &&
+                                    (+feature.properties.description < magBins[i]));
+                        },
+                        pointToLayer: function (feature, latlng) {
+                            return L.circleMarker(latlng, {
+                            fillColor: colorScale(+feature.geometry),
+                            color: '#000',
+                            weight: 1,
+                            opacity: 1,
+                            fillOpacity: 0.9,
+                        });
+                        },
+                        onEachFeature: addPopupInfo,
+                    });
+    
+            // Create a label for the magnitude bin and add the layer to eqLayers.
+            var lvlKey = magBins[i] + (magBins[i + 1] ? '-' + magBins[i + 1] : '+');
+            eqLayers[lvlKey] = eqLayer;
+        };
+    
+        // Create an overlay layer for the earthquake timeline.
+        var timelineLayer = new L.Timeline(geojsonData, {
+                    getInterval: function (quake) {
+                        return ({
+                            start: quake.properties.time,
+                            end:   quake.properties.time + 86400000,
+                                });
+                    },
+                });
+    
+        // Pass the earthquake overlay layers, the timeline overlay layer and the
+        // legend to the createMap() function.
+        //
+        createMap(eqLayers, timelineLayer, legend);
+
+    }, (reason) => {
+        console.log(reason);
+    });
 }
+
+function init() {
+    mapWarnings();
+}
+
+init();
